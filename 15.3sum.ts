83f1b5579bcca7271b58e0498579b7aacfdb5d9f
Talal Balnoob <TalalBalnoob@gmail.com>
2026-03-08 00:36:05 +0300

    try 15

--- 15.3sum.ts
+++ 15.3sum.ts
@@ -1,37 +1,24 @@
 function threeSum(nums: number[]): number[][] {
-  let res: number[][] = []
+  let results: number[][] = []
 
-  let numbers = nums.map((nums) => nums)
-
-  for (let i = 0; i < nums.length; i++) {
-    let number1 = nums[i]
-
-    for (let j = i + 1; j < nums.length; j++) {
-      // if (j === i) continue
-
-      let number2 = nums[j]
-
-      for (let k = j + 1; k < nums.length; k++) {
-        // if (i === k || j === k) continue
-        let number3 = nums[k]
-
-        if (number1 + number2 + number3 === 0)
-          res = [...res, [number1, number2, number3]]
-      }
-    }
+  for (let i = 2; i < nums.length; i += 2) {
+    let res = twoSum([...nums.slice(i - 1, i + 1)], -nums[i - 2])
+    if (res.length > 0)
+      results.push([...res.map((num) => nums[num]), nums[i - 2]])
   }
 
-  let uniqueRes = res.map((item) => item.sort((a, b) => a - b))
-  uniqueRes = uniqueRes.filter(
-    (item, index) =>
-      uniqueRes.findIndex(
-        (i) => i[0] === item[0] && i[1] === item[1] && i[2] === item[2],
-      ) === index,
-  )
+  return results
+}
 
+function twoSum(nums: number[], target: number): number[] {
+  let map = new Map()
 
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) return [i, map.get(target - nums[i])]
+    else map.set(nums[i], i)
+  }
 
-  return uniqueRes
+  return []
 }
 
 threeSum([-1, 0, 1, 2, -1, -4])
