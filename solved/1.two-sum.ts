2413d47da4a2f9333afe7b34b5edf26c3e1c3eed
Talal Balnoob <TalalBalnoob@gmail.com>
2026-03-07 23:25:46 +0300

    improve 1

--- solved/1.two-sum.ts
+++ solved/1.two-sum.ts
@@ -1,9 +1,21 @@
+// function twoSum(nums: number[], target: number): number[] {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       var res = nums[i] + nums[j]
+//       if (res === target) return [i, j]
+//     }
+//   }
+//   return []
+// }
+
+// improve
 function twoSum(nums: number[], target: number): number[] {
+  let map = new Map()
+
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      var res = nums[i] + nums[j]
-      if (res === target) return [i, j]
-    }
+    if (map.has(target - nums[i])) return [i, map.get(target - nums[i])]
+    else map.set(nums[i], i)
   }
+
   return []
 }
