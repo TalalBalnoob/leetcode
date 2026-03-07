efaa73947d183f1fd66d077b7ed5a30bfd06945e
Talal Balnoob <TalalBalnoob@gmail.com>
2026-03-08 00:36:24 +0300

    _

--- solved/1.two-sum.ts
+++ solved/1.two-sum.ts
@@ -9,13 +9,13 @@
 // }
 
 // improve
-// function twoSum(nums: number[], target: number): number[] {
-//   let map = new Map()
+function twoSum(nums: number[], target: number): number[] {
+  let map = new Map()
 
-//   for (let i = 0; i < nums.length; i++) {
-//     if (map.has(target - nums[i])) return [i, map.get(target - nums[i])]
-//     else map.set(nums[i], i)
-//   }
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) return [i, map.get(target - nums[i])]
+    else map.set(nums[i], i)
+  }
 
-//   return []
-// }
+  return []
+}
